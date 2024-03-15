b2c103a847447f02d6e3708f00c343b6ed948954
joergi <1439809+joergi@users.noreply.github.com>
2024-03-15 15:42:12 +0100

    First commit

---
+++ apache-beam-dependency-problem/src/main/kotlin/io/joergi/apachebeamdependencyproblem/ApacheBeamDependencyProblemApplication.kt
@@ -0,0 +1,11 @@
+package io.joergi.apachebeamdependencyproblem
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ApacheBeamDependencyProblemApplication
+
+fun main(args: Array<String>) {
+	runApplication<ApacheBeamDependencyProblemApplication>(*args)
+}
